a7f2a491e1497671b480a77ecc0301fafd39140d
huhuaping <huhuaping01@nwsuaf.edu.cn>
2025-01-06 10:14:16 +0800

    scrape dragon year 2024 finished

--- code/scrape-04-dragon-pool.R
+++ code/scrape-04-dragon-pool.R
@@ -16,6 +16,7 @@ library(stringr)
 library(lubridate)
 library(here)
 library(glue)
+library(jsonlite)
 
 # 2. 启动docker
 ## run docker service and container
@@ -29,7 +30,7 @@ library(glue)
 ## 输入端口密码
 
 
-# 4. 连接selenium
+# 2. 连接selenium====
 remDr <- remoteDriver(remoteServerAddr = "localhost",
                        port = 4445L,
                        browserName = "chrome")
@@ -37,10 +38,28 @@ remDr <- remoteDriver(remoteServerAddr = "localhost",
 remDr$open()
 remDr$maxWindowSize()
 
+# 3. 读取法定节假日json数据====
+## 3.1 读取json数据
+file_json <- here("data", "holiday-new.json")
+# Read the JSON file
+json_data <- fromJSON(file_json)
+
+## step3: Convert to tibble and rename columns
+names_new <- c("date","stock_id", "market", "stock_name")
+tbl_holiday <- json_data %>%
+  imap_dfr(~ tibble(
+    year = .y,
+    holiday = .x
+  )
+  ) %>%
+  unnest(holiday) %>%
+  unnest(date) %>%
+  rename("holiday" = "name") 
+
 # 4. 设定目标日期====
-date_start <- ymd("2024-12-01")
-date_end <- ymd("2024-12-31")
-list_holiday <- NA
+date_start <- ymd("2024-01-01")
+date_end <- ymd("2024-09-30")
+#list_holiday <- NA
 tbl_date <- tibble(date = as.Date(date_start:date_end)) %>%
   mutate(
     wday = wday(date, week_start = 1),
@@ -48,7 +67,7 @@ tbl_date <- tibble(date = as.Date(date_start:date_end)) %>%
   ) %>%
   mutate(
     is.weekend = wday %in% c("周六", "周日"),
-    is.holiday = date %in% as.Date(list_holiday)
+    is.holiday = date %in% as.Date(tbl_holiday$date)
   ) %>%
   filter(!is.weekend, !is.holiday)
 #target_date <- "2023-12-14"
@@ -56,8 +75,8 @@ tbl_date <- tibble(date = as.Date(date_start:date_end)) %>%
 # 5. 抓取龙虎榜日度榜单数据====
 
 ## loop all dates
-k <- 10
-for (k in 22:nrow(tbl_date)){
+k <- 29
+for (k in 124:nrow(tbl_date)){
   cat(glue("begin {k} of total {nrow(tbl_date)} \n"))
   
   ## 5.0 设定目标日期====
@@ -65,14 +84,16 @@ for (k in 22:nrow(tbl_date)){
   ## 5.1 进入龙虎榜数据中心====
   url_tar <- "https://data.eastmoney.com/stock/tradedetail.html"
   remDr$navigate(url_tar)
-  Sys.sleep(4)
+  Sys.sleep(2)
   
   ## 5.2 设定日期====
   ## 点击自定义区间
-  css_tar <- "li.tagRange"
+  #css_tar <- "li.tagRange"
+  css_tar <- "body > div:nth-child(2) > div.main-content > div.content > div:nth-child(3) > ul > li.tagRange"
   elm <- remDr$findElement(using = "css", value = css_tar)
-  elm$clickElement()
   Sys.sleep(2)
+  elm$clickElement()
+  
   
   ## 5.3 开始日期选择器====
   ## we have to select the date range in the calendar with the date picker.
@@ -81,10 +102,10 @@ for (k in 22:nrow(tbl_date)){
   elm <- remDr$findElement(using = "css", value = css_tar)
   ## send target date to the date picker
   elm$clickElement()
-  Sys.sleep(2)
+  Sys.sleep(1)
   # clear the date picker
   remDr$executeScript("arguments[0].value = '';", list(elm))
-  Sys.sleep(2) # larger if the network is slow
+  Sys.sleep(1) # larger if the network is slow
   # send new date to the date picker
   elm$sendKeysToElement(list(target_date))
   ## get the date
@@ -126,32 +147,51 @@ for (k in 22:nrow(tbl_date)){
   ## 5.7 获取总页码数====
   ## the html source is: 
   css_tar <- "div.pagerbox >a"
-  page_total <- html_nodes(page, css_tar) %>% 
-    html_text() %>% # may be NA for "下一页"
-    as.numeric() %>% 
-    na.omit() %>% 
-    max()
-  cat("Total pages: ", page_total, "\n")
+  ## check element exist
+  if (length(html_nodes(page, css_tar)) == 0) {
+    cat("No data for ", target_date, "\n")
+    warning("No paganation for ", target_date, "\n")
+    page_total <- 999
+  } else {
+    page_total <- html_nodes(page, css_tar) %>% 
+      html_text() %>% # may be NA for "下一页"
+      as.numeric() %>% 
+      na.omit() %>% 
+      max()
+    cat("Total pages: ", page_total, "\n")
+  }
   
   # 6. 循环抓取所有页数据====
   ## 6.1 循环抓取所有页数据
   ## initialize
   tbl_all <- tibble()
+  
+  ## reset total pages
+  if (page_total == 999) {
+    total_pages <- 1
+  } else {
+    total_pages <- page_total
+  }
+  
   #i <- 3
-  for (i in 1:page_total) {
-    ## fill the page index
-    css_tar <- "#gotopageindex"
-    elm <- remDr$findElement(using = "css", value = css_tar)
-    ## clear the page index
-    elm$clearElement()
-    ## send new page index
-    elm$sendKeysToElement(list(as.character(i)))
-    Sys.sleep(1)
-    ## click the ok button
-    css_tar <- "input.btn"
-    elm <- remDr$findElement(using = "css", value = css_tar)
-    elm$clickElement()
-    Sys.sleep(1)
+  for (i in 1:total_pages) {
+    # case of multiple page
+    if (page_total != 999) {
+      ## fill the page index
+      css_tar <- "#gotopageindex"
+      elm <- remDr$findElement(using = "css", value = css_tar)
+      ## clear the page index
+      elm$clearElement()
+      ## send new page index
+      elm$sendKeysToElement(list(as.character(i)))
+      Sys.sleep(1)
+      ## click the ok button
+      css_tar <- "input.btn"
+      elm <- remDr$findElement(using = "css", value = css_tar)
+      elm$clickElement()
+      Sys.sleep(1)
+    }
+    
     ## read page
     page <- remDr$getPageSource()[[1]]
     Sys.sleep(1)
@@ -182,7 +222,9 @@ for (k in 22:nrow(tbl_date)){
   cat("Date ", target_date, " is done.\n")
   
   ## Force garbage collection (optional, but good to do)
-  gc()
+  #gc()
+  # close browser and to the blank page
+  remDr$navigate("about:blank")
 }
 
 ## close the driver====
